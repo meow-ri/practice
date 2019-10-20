0723566c37251f75af19393ff837a0ad209e4fe4
meow-ri <umba.araika19@gmail.com>
2019-10-21 00:15:50 +0600

    optimization

---
+++ raid01/meow-ri/raid01d.go
@@ -0,0 +1,44 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Raid1d(x, y int) {
+
+	var topLeft rune = 'A'
+	var topRight rune = 'C'
+	var bottomLeft rune = 'A'
+	var bottomRight rune = 'C'
+	var vert rune = 'B'
+	var hori rune = 'B'
+
+	for j := 1; j <= y; j++ { //j is number of line
+		for i := 1; i <= x; i++ { //i is number of column
+			if j == 1 { //if it is top line
+				if i == 1 { //if it is left column
+					z01.PrintRune(topLeft)
+				} else if i == x {
+					z01.PrintRune(topRight)
+				} else {
+					z01.PrintRune(hori)
+				}
+			} else if j == y {
+				if i == 1 { //if it is left column
+					z01.PrintRune(bottomLeft)
+				} else if i == x {
+					z01.PrintRune(bottomRight)
+				} else {
+					z01.PrintRune(hori)
+				}
+			} else {
+				if i == 1 { //if it is left column
+					z01.PrintRune(vert)
+				} else if i == x {
+					z01.PrintRune(vert)
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
